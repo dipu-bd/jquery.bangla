cc1416221033d0252e52fa0d61680c4f49a9807d
Sudipto Chandra <dipu.sudipta@gmail.com>
2018-07-30 02:39:18 +0600

    Fix the suggest position when scrolling

--- src/index.js
+++ src/index.js
@@ -42,9 +42,16 @@ function insertAtCursor ($elem, text) {
 }
 
 function registerInputTool ($elem, v) {
+  // modify configs
   if (v) console.log('config', v)
-  let isBN = true
 
+  // Necessary variables
+  let isBN = true
+  let word = ''
+  let suggestions = []
+  let selectedIndex = -1
+
+  // Initialize avro
   const avro = AvroPhonetic(
     function () {
       return JSON.parse(KVStore.getItem(STORE_KEY) || '{}')
@@ -54,36 +61,31 @@ function registerInputTool ($elem, v) {
     }
   )
 
-  let word = ''
-  let suggestions = []
-  let selectedIndex = -1
-
-  const updateView = () => {
+  // Build the current view
+  const buildView = () => {
     // get or generate view
-    if (!window.document.getElementById('bangla--suggestions')) {
-      const view = $(`<div id="bangla-suggestions">&nbsp;</div>`)
+    const id = 'bangla--suggestions'
+    if (!window.document.getElementById(id)) {
+      const view = $(`<div id="${id}">&nbsp;</div>`)
       $('body').append(view)
       view.css({
         'font-size': $elem.css('font-size') || (16 + 'px'),
         'display': 'none',
-        'z-index': '10000',
+        'z-index': 10000,
         'position': 'fixed',
+        'padding': '1px',
         'min-width': '200px',
         'background': '#f7f7f9',
         'border-radius': '3px',
-        'border': '1px solid #dcdcdf',
+        'border': '1px solid #d6d9db',
         'box-shadow': '0 0 5px 0 rgba(#000, 0.18), 0 0 0 1px rgba(#000, 0.12)'
       })
     }
-    const view = $('#bangla-suggestions')
+    return $(`#${id}`)
+  }
 
-    // check if in need of view
-    view.html('')
-    if (!word) {
-      return view.css('display', 'none')
-    }
-
-    // add current running word
+  // Builds the current word view
+  const buildRunningItem = (view) => {
     const running = $(`<div>${word}</div>`)
     running.css({
       'color': 'navy',
@@ -93,12 +95,18 @@ function registerInputTool ($elem, v) {
       'margin-bottom': 0,
       'padding': '5px 12px',
       'background': 'white',
-      'border-bottom': '1px solid #dedfe5'
+      'overflow': 'visible',
+      'border-bottom': '1px solid #dedfe5',
+      'transform': 'translateZ(0)',
+      '-moz-transform': 'translateZ(0)',
+      '-webkit-transform': 'translateZ(0)'
     })
     view.append(running)
+  }
 
-    // generate suggestion list
-    suggestions.slice(0, 10).map((val, index) => {
+  // Build the list items
+  const buildListItems = (view) => {
+    suggestions.slice(0, 10).forEach((val, index) => {
       const item = $(`<div>${val}</div>`)
       item.css({
         'cursor': 'pointer',
@@ -113,36 +121,68 @@ function registerInputTool ($elem, v) {
       }
       item.click(() => {
         selectedIndex = index
-        selectCurrentWord()
+        commitCurrentWord()
       })
       view.append(item)
     })
+  }
+
+  // Udpates the view
+  const updateView = () => {
+    // get or generate view
+    const view = buildView()
+    view.html('')
+
+    // check if we need to show view
+    if (!word) {
+      return view.css('display', 'none')
+    }
+
+    // add current running word
+    buildRunningItem(view)
+
+    // generate suggestion list
+    buildListItems(view)
 
     // display the view
     const offset = $elem.caret('offset')
+    let top = (offset.top + offset.height - window.scrollY + 3) + 'px'
+    // const height = offset.top + view.height() + 5
+    // if (height > window.innerHeight) {   // check if the view should be on top
+    //   top = (offset.top - window.scrollY - view.height() - 7) + 'px'
+    //   view.append(running)
+    // }
     view.css({
       display: 'block',
-      top: (offset.top + offset.height + 5) + 'px',
-      left: offset.left + 'px'
+      top,
+      left: (offset.left) + 'px'
     })
+    // console.log(offset.left, offset.top)
   }
 
-  const buildSuggestion = function (e) {
-    if (!word.length) return
-    const past = avro.candidate(word)
-    suggestions = avro.suggest(word).words
-    selectedIndex = suggestions.indexOf(past)
-    if (selectedIndex < 0 && suggestions.length) {
-      selectedIndex = 0
+  // Builds the suggestion list
+  const buildSuggestions = function (e) {
+    if (word) {
+      const past = avro.candidate(word)
+      suggestions = avro.suggest(word).words
+      selectedIndex = suggestions.indexOf(past)
+      if (selectedIndex < 0 && suggestions.length) {
+        selectedIndex = 0
+      }
+    } else {
+      suggestions = []
+      selectedIndex = -1
     }
   }
 
+  // Updates the current word
   const setWord = (newWord) => {
     word = newWord || ''
-    if (word) buildSuggestion()
+    buildSuggestions()
     updateView()
   }
 
+  // Sets the selected index
   const setSelectedIndex = (index) => {
     if (suggestions.length > 0) {
       selectedIndex = (index + suggestions.length) % suggestions.length
@@ -152,7 +192,8 @@ function registerInputTool ($elem, v) {
     updateView()
   }
 
-  const selectCurrentWord = () => {
+  // Commit the current word
+  const commitCurrentWord = () => {
     let selected = word
     if (selectedIndex >= 0 || selectedIndex < suggestions.length) {
       selected = suggestions[selectedIndex]
@@ -163,18 +204,45 @@ function registerInputTool ($elem, v) {
     setWord('')
   }
 
+  //
+  // Watch on window Resize
+  //
+  $(window).resize((e) => {
+    if (isBN && word) updateView()
+  })
+
+  //
+  // Watch on window OnScroll
+  //
+  $(window).scroll((e) => {
+    if (isBN && word) updateView()
+  })
+
+  //
+  // Watch on window KeyDown
+  //
+  $(window).keydown((e) => {
+    if (e.keyCode === KEY_CODE.ESC && word) {
+      setWord('')
+      return false
+    }
+  })
+
+  //
+  // Watch on element KeyDown
+  //
   $elem.keydown((e) => {
     if (e.ctrlKey && [KEY_CODE.DOT, KEY_CODE.SPACE].indexOf(e.keyCode) >= 0) {
       isBN = !isBN
     }
-    if (!isBN) return true
-
     switch (e.keyCode) {
       case KEY_CODE.UP:
+        if (!word) return true
         setSelectedIndex(selectedIndex - 1)
         if (selectedIndex >= 0) return false
         break
       case KEY_CODE.DOWN:
+        if (!word) return true
         setSelectedIndex(selectedIndex + 1)
         if (selectedIndex >= 0) return false
         break
@@ -203,7 +271,7 @@ function registerInputTool ($elem, v) {
     }
 
     if (e.key !== 'Shift' && word.length) {
-      selectCurrentWord()
+      commitCurrentWord()
       if ([KEY_CODE.ENTER, KEY_CODE.TAB].indexOf(e.keyCode) >= 0) {
         return false
       }
