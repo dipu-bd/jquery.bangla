f7ed475142d39d92e7267b6541bfd73046e32a49
Sudipto Chandra <dipu.sudipta@gmail.com>
2018-07-29 14:27:41 +0600

    Add es6 build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import './avro.min.js'
-
 export default function register($elem) {
+  const avro = window.AvroPhonetic(
+    function() {
+      return {}
+    },
+    function(candidates) {
+      console.log(candidates)
+    }
+  )
+
 
 }
