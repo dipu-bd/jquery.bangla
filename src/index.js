2ac79fee97156c50e3e5a3dc67d2d18cf585ef94
Sudipto Chandra <dipu.sudipta@gmail.com>
2018-07-29 15:17:48 +0600

    Import avro phonetic success

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-export default function register($elem) {
-  const avro = window.AvroPhonetic(
-    function() {
+import AvroPhonetic from './lib/avro.min.js'
+
+function registerInputTool ($elem) {
+  const avro = AvroPhonetic(
+    () => {
       return {}
     },
-    function(candidates) {
+    (candidates) => {
       console.log(candidates)
     }
   )
 
-
+  console.log(avro.suggest('bangla'))
+  // avro.commit('bangla', selectedWord)
+  // avro.candidate('bangla')
 }
+
+const jquery = window.$ || window.jQuery
+if (jquery) {
+  jquery.fn.initBangla = function () {
+    return this.each(() => registerInputTool(jquery(this)))
+  }
+}
+
+export default registerInputTool
