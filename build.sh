18710f0056aba8d865adf5564f6a36a17cecbb4d
dipu-bd <dipu.sudipta@gmail.com>
2019-02-09 15:11:30 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ git pull origin gh-pages
 npm remove -S jquery.bangla
 npm install -S jquery.bangla
 cp -r node_modules/jquery.bangla/dist .
-git add --all
+git add dist/ package.json package-lock.json
 git commit -m "Update package"
 git push origin gh-pages
