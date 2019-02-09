5229cb5f162f55601a30e4183e2037cdf3c33a0f
dipu-bd <dipu.sudipta@gmail.com>
2019-02-09 15:09:37 +0600

    Update package

---
+++ build.sh
@@ -0,0 +1,7 @@
+git checkout gh-pages
+git pull origin gh-pages
+npm remove -S jquery.bangla
+npm install -S jquery.bangla
+cp -r node_modules/jquery.bangla/dist .
+git add --all
+git commit -m "Update package"
